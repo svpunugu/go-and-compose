d52c88d0ea6fe78886612b34d85fb24dae5d586d
svpunugu <sunniz31@gmail.com>
2022-09-29 15:06:23 -0400

    Initial commit

---
+++ apiserver/items.go
@@ -0,0 +1,36 @@
+// apiserver/items.go
+package apiserver
+
+import (
+  "fmt"
+  "net/http"
+
+  "github.com/svpunugu/go-and-compose/storage"
+)
+
+func (s *APIServer) createItem(w http.ResponseWriter, req *http.Request) error {
+       item, err := s.storage.CreateItem(req.Context(), storage.CreateItemRequest{
+               Name: req.PostFormValue("name"),
+       })
+
+       if err != nil {
+               return err
+       }
+
+       w.WriteHeader(http.StatusCreated)
+       _, err = w.Write([]byte(fmt.Sprintf("New Item ID: %s", item.ID)))
+       return err
+}
+
+func (s *APIServer) listItems(w http.ResponseWriter, req *http.Request) error {
+       items, err := s.storage.ListItems(req.Context())
+       if err != nil {
+               return err
+       }
+
+       for _, item := range items {
+               w.Write([]byte(fmt.Sprintf("%s - %s\n", item.ID, item.Name)))
+       }
+   return nil
+}
+
---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/svpunugu/go-and-compose
+
+go 1.16
+
+require (
+	github.com/gorilla/mux v1.8.0
+	github.com/lib/pq v1.10.7
+	github.com/sirupsen/logrus v1.9.0
+	github.com/urfave/cli/v2 v2.17.1
+)
